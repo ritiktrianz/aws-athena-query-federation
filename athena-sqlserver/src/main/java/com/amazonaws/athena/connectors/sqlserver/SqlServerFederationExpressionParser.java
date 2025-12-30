0f3349b437eb7f7a6271932b1c58128fe123a9ac
ritik <ritik.verma@trianz.com>
2025-12-30 12:07:14 +0530

    centralized string template for jdbc connectors.

--- athena-sqlserver/src/main/java/com/amazonaws/athena/connectors/sqlserver/SqlServerFederationExpressionParser.java
+++ athena-sqlserver/src/main/java/com/amazonaws/athena/connectors/sqlserver/SqlServerFederationExpressionParser.java
@@ -26,6 +26,7 @@ import com.amazonaws.athena.connector.lambda.domain.predicate.functions.Function
 import com.amazonaws.athena.connector.lambda.domain.predicate.functions.OperatorType;
 import com.amazonaws.athena.connector.lambda.domain.predicate.functions.StandardFunctions;
 import com.amazonaws.athena.connectors.jdbc.manager.JdbcFederationExpressionParser;
+import com.amazonaws.athena.connectors.jdbc.manager.JdbcSqlUtils;
 import com.amazonaws.athena.connectors.jdbc.manager.TypeAndValue;
 import org.apache.arrow.vector.complex.reader.FieldReader;
 import org.apache.arrow.vector.types.Types;
@@ -54,7 +55,7 @@ public class SqlServerFederationExpressionParser extends JdbcFederationExpressio
     @Override
     public String writeArrayConstructorClause(ArrowType type, List<String> arguments)
     {
-        return SqlServerSqlUtils.renderTemplate("comma_separated_list_with_parentheses", Map.of("items", arguments));
+        return JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "comma_separated_list_with_parentheses", Map.of("items", arguments));
     }
 
     @Override
@@ -79,7 +80,7 @@ public class SqlServerFederationExpressionParser extends JdbcFederationExpressio
                     .collect(Collectors.toList());
         }
 
-        return SqlServerSqlUtils.renderTemplate("comma_separated_list", Map.of("items", constants));
+        return JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "comma_separated_list", Map.of("items", constants));
     }
 
     @Override
@@ -111,67 +112,67 @@ public class SqlServerFederationExpressionParser extends JdbcFederationExpressio
         String clause = "";
         switch (functionEnum) {
             case ADD_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("join_expression", Map.of("items", arguments, "separator", " + "));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "join_expression", Map.of("items", arguments, "separator", " + "));
                 break;
             case AND_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("join_expression", Map.of("items", arguments, "separator", " AND "));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "join_expression", Map.of("items", arguments, "separator", " AND "));
                 break;
             case ARRAY_CONSTRUCTOR_FUNCTION_NAME: // up to subclass
                 clause = writeArrayConstructorClause(type, arguments);
                 break;
             case DIVIDE_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("join_expression", Map.of("items", arguments, "separator", " / "));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "join_expression", Map.of("items", arguments, "separator", " / "));
                 break;
             case EQUAL_OPERATOR_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("join_expression", Map.of("items", arguments, "separator", " = "));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "join_expression", Map.of("items", arguments, "separator", " = "));
                 break;
             case GREATER_THAN_OPERATOR_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("join_expression", Map.of("items", arguments, "separator", " > "));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "join_expression", Map.of("items", arguments, "separator", " > "));
                 break;
             case GREATER_THAN_OR_EQUAL_OPERATOR_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("join_expression", Map.of("items", arguments, "separator", " >= "));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "join_expression", Map.of("items", arguments, "separator", " >= "));
                 break;
             case IN_PREDICATE_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("in_expression", Map.of("column", arguments.get(0), "values", arguments.get(1)));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "in_expression", Map.of("column", arguments.get(0), "values", arguments.get(1)));
                 break;
             case IS_DISTINCT_FROM_OPERATOR_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("is_distinct_from", Map.of("left", arguments.get(0), "right", arguments.get(1)));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "is_distinct_from", Map.of("left", arguments.get(0), "right", arguments.get(1)));
                 break;
             case IS_NULL_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("null_predicate", Map.of("columnName", arguments.get(0), "isNull", true));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "null_predicate", Map.of("columnName", arguments.get(0), "isNull", true));
                 break;
             case LESS_THAN_OPERATOR_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("join_expression", Map.of("items", arguments, "separator", " < "));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "join_expression", Map.of("items", arguments, "separator", " < "));
                 break;
             case LESS_THAN_OR_EQUAL_OPERATOR_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("join_expression", Map.of("items", arguments, "separator", " <= "));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "join_expression", Map.of("items", arguments, "separator", " <= "));
                 break;
             case LIKE_PATTERN_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("like_expression", Map.of("column", arguments.get(0), "pattern", arguments.get(1)));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "like_expression", Map.of("column", arguments.get(0), "pattern", arguments.get(1)));
                 break;
             case MODULUS_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("function_call_2args", Map.of("functionName", "MOD", "arg1", arguments.get(0), "arg2", arguments.get(1)));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "function_call_2args", Map.of("functionName", "MOD", "arg1", arguments.get(0), "arg2", arguments.get(1)));
                 break;
             case MULTIPLY_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("join_expression", Map.of("items", arguments, "separator", " * "));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "join_expression", Map.of("items", arguments, "separator", " * "));
                 break;
             case NEGATE_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("unary_operator", Map.of("operator", "-", "operand", arguments.get(0)));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "unary_operator", Map.of("operator", "-", "operand", arguments.get(0)));
                 break;
             case NOT_EQUAL_OPERATOR_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("join_expression", Map.of("items", arguments, "separator", " <> "));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "join_expression", Map.of("items", arguments, "separator", " <> "));
                 break;
             case NOT_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("unary_operator", Map.of("operator", "NOT ", "operand", arguments.get(0)));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "unary_operator", Map.of("operator", "NOT ", "operand", arguments.get(0)));
                 break;
             case NULLIF_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("function_call_2args", Map.of("functionName", "NULLIF", "arg1", arguments.get(0), "arg2", arguments.get(1)));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "function_call_2args", Map.of("functionName", "NULLIF", "arg1", arguments.get(0), "arg2", arguments.get(1)));
                 break;
             case OR_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("join_expression", Map.of("items", arguments, "separator", " OR "));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "join_expression", Map.of("items", arguments, "separator", " OR "));
                 break;
             case SUBTRACT_FUNCTION_NAME:
-                clause = SqlServerSqlUtils.renderTemplate("join_expression", Map.of("items", arguments, "separator", " - "));
+                clause = JdbcSqlUtils.renderTemplate(SqlServerSqlUtils.getQueryFactory(), "join_expression", Map.of("items", arguments, "separator", " - "));
                 break;
             default:
                 throw new NotImplementedException("The function " + functionName.getFunctionName() + " does not have an implementation");
