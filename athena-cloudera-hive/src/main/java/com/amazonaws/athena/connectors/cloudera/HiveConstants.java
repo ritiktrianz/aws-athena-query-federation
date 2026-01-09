b478ccdb3568e260641d12c78c39c8ff94b4359d
Jithendar12 <jithendar.vallepu@trianz.com>
2026-01-09 16:09:10 +0530

    Use StringTemplate to build SQL in athena-cloudera-hive connector

--- athena-cloudera-hive/src/main/java/com/amazonaws/athena/connectors/cloudera/HiveConstants.java
+++ athena-cloudera-hive/src/main/java/com/amazonaws/athena/connectors/cloudera/HiveConstants.java
@@ -30,10 +30,10 @@ public class HiveConstants
     }
 
     static final Map<String, String> JDBC_PROPERTIES = ImmutableMap.of("databaseTerm", "SCHEMA");
-    static final String BLOCK_PARTITION_COLUMN_NAME = "partition";
+    public static final String BLOCK_PARTITION_COLUMN_NAME = "partition";
     static final int MAX_SPLITS_PER_REQUEST = 1000_000;
     static final String COLUMN_NAME = "COLUMN_NAME";
-    static final String HIVE_QUOTE_CHARACTER = "";
+    public static final String HIVE_QUOTE_CHARACTER = "";
     static final int FETCH_SIZE = 1000;
     static final String ALL_PARTITIONS = "*";
     public static final String HIVE_NAME = "hive";
--- athena-cloudera-hive/src/main/java/com/amazonaws/athena/connectors/cloudera/HiveFederationExpressionParser.java
+++ athena-cloudera-hive/src/main/java/com/amazonaws/athena/connectors/cloudera/HiveFederationExpressionParser.java
@@ -19,13 +19,15 @@
  */
 package com.amazonaws.athena.connectors.cloudera;
 
-import com.amazonaws.athena.connectors.jdbc.manager.JdbcFederationExpressionParser;
-import com.google.common.base.Joiner;
-import org.apache.arrow.vector.types.pojo.ArrowType;
+import com.amazonaws.athena.connectors.jdbc.manager.JdbcQueryFactory;
+import com.amazonaws.athena.connectors.jdbc.manager.TemplateBasedJdbcFederationExpressionParser;
 
-import java.util.List;
-
-public class HiveFederationExpressionParser extends JdbcFederationExpressionParser
+/**
+ * Hive implementation of FederationExpressionParser using StringTemplate.
+ * Extends TemplateBasedJdbcFederationExpressionParser which provides the common
+ * template-based implementation for all migrated JDBC connectors.
+ */
+public class HiveFederationExpressionParser extends TemplateBasedJdbcFederationExpressionParser
 {
     public HiveFederationExpressionParser(String quoteChar)
     {
@@ -33,8 +35,8 @@ public class HiveFederationExpressionParser extends JdbcFederationExpressionPars
     }
 
     @Override
-    public String writeArrayConstructorClause(ArrowType type, List<String> arguments)
+    protected JdbcQueryFactory getQueryFactory()
     {
-        return Joiner.on(", ").join(arguments);
-    }    
+        return HiveSqlUtils.getQueryFactory();
+    }
 }
